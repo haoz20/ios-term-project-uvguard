f86d9040ae579a4b8de454afc6c4411dec07800f
haoz20 <zhaonianhao2005@gmail.com>
2025-08-21 00:13:55 +0700

    Initial Commit

---
+++ UVGuard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UVGuard
+//
+//  Created by Swan Htet Aung on 21/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UVGuard/UVGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  UVGuardApp.swift
+//  UVGuard
+//
+//  Created by Swan Htet Aung on 21/8/25.
+//
+
+import SwiftUI
+
+@main
+struct UVGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
