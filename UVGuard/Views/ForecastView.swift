bc37885af7e8a8ba95f1db71e8dc8a1ffed1835e
haoz20 <zhaonianhao2005@gmail.com>
2025-09-04 14:23:46 +0700

    chore: add ForecastView file

---
+++ UVGuard/Views/ForecastView.swift
@@ -0,0 +1,18 @@
+//
+//  ForecastView.swift
+//  UVGuard
+//
+//  Created by Swan Htet Aung on 4/9/25.
+//
+
+import SwiftUI
+
+struct ForecastView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForecastView()
+}
