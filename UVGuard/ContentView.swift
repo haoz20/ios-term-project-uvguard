996f949bf647ed3d9b3cc08f29e1b4cb0c1c1fa4
haoz20 <zhaonianhao2005@gmail.com>
2025-09-07 00:40:49 +0700

    fix: remove padding modifier for the ContentView

--- UVGuard/ContentView.swift
+++ UVGuard/ContentView.swift
@@ -10,12 +10,9 @@ import SwiftUI
 struct ContentView: View {
 //    @StateObject var locationDataManager = LocationDataManager()
     var body: some View {
-        VStack {
 //            Text("Latitude: \(locationDataManager.locationManager.location?.coordinate.latitude.description ?? "Error loading")")
 //            Text("Longitude: \(locationDataManager.locationManager.location?.coordinate.longitude.description ?? "Error loading")")
             RootTabView()
-        }
-        .padding()
     }
 }
 
